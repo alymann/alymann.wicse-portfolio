aabc081c255be729d1969b13d53efb961b3dbf3d
Alyssa Mann <a.mann@ufl.edu>
2025-02-19 22:28:53 -0500

    minor details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import pic from './assets/aly.png'
 import Navbar from './pages/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
